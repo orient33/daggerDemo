b987df544b27da208cd36a613fdc4384dbb78473
dfdun <orient33@163.com>
2019-04-23 14:48:03 +0800

    updage gradle plugin 3.4

--- app/src/main/java/com/example/daggerdemo/di/AppComponent.java
+++ app/src/main/java/com/example/daggerdemo/di/AppComponent.java
@@ -8,13 +8,13 @@ import javax.inject.Singleton;
 
 import dagger.BindsInstance;
 import dagger.Component;
+import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
-import dagger.android.support.AndroidSupportInjectionModule;
 
 @Singleton
 @Component(modules = {ApplicationModule.class,
         ActivityBindModule.class,
-        AndroidSupportInjectionModule.class})
+        AndroidInjectionModule.class})
 public interface AppComponent extends AndroidInjector<App> {
 
     @Component.Builder
